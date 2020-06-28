d1b24aa8d35ac56f11c4363fa4bc3633c5813519
Gustavo Cotta <gmcotta34@gmail.com>
2020-06-28 20:23:14 -0300

    Inital config - editorconfig, eslint, prettier

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import routes from './routes';
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (request, response) => response.json({ message: 'Hello Teste!' }));
+
+app.listen(3333, () => {
+  console.log('🚀 Server started on port 3333!');
+});
