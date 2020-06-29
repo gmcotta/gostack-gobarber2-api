bcdce6ebc9efcb0ded3b421001ebe38dd14c8a9d
Gustavo Cotta <gmcotta34@gmail.com>
2020-06-28 21:07:15 -0300

    Creating Appointments

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import routes from './routes';
 const app = express();
 app.use(express.json());
 
-app.get('/', (request, response) => response.json({ message: 'Hello Teste!' }));
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('🚀 Server started on port 3333!');
