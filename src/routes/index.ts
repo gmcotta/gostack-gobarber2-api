43fbf16dce1888a5aa9f82016e9d35d9ed7d2672
Gustavo Cotta <gmcotta34@gmail.com>
2020-07-05 18:10:12 -0300

    Serving Static Files

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,13 @@
-import { Router } from 'express';
+import express, { Router } from 'express';
 
 import appointmentsRouter from './appointments.routes';
 import usersRouter from './users.routes';
 import sessionsRouter from './sessions.routes';
+import uploadConfig from '../config/upload';
 
 const routes = Router();
 
+routes.use('/files', express.static(uploadConfig.directory));
 routes.use('/appointments', appointmentsRouter);
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
