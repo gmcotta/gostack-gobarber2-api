cf3acfcc3a68b5e1938226b3317c0304c121be90
Gustavo Cotta <gmcotta34@gmail.com>
2020-07-12 15:43:17 -0300

    Validating fields with Celebrate

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import multer from 'multer';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 import uploadConfig from '@config/upload';
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
@@ -13,7 +14,17 @@ const userAvatarController = new UserAvatarController();
 
 usersRouter.get('/', userController.index);
 
-usersRouter.post('/', userController.create);
+usersRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    },
+  }),
+  userController.create,
+);
 
 usersRouter.patch(
   '/avatar',
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
+import { errors } from 'celebrate';
 
 import AppError from '@shared/errors/AppError';
 import routes from './routes';
@@ -12,6 +13,9 @@ const app = express();
 app.use(express.json());
 
 app.use(routes);
+
+app.use(errors());
+
 app.use(
   (error: Error, request: Request, response: Response, _: NextFunction) => {
     if (error instanceof AppError) {
