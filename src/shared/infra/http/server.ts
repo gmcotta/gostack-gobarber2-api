301ad5a044b55ad57d09402286336647402c49bf
Gustavo Cotta <gmcotta34@gmail.com>
2020-07-13 09:08:11 -0300

    Adding rate limiter middleware

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -2,21 +2,24 @@ import 'reflect-metadata';
 import 'dotenv/config';
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
+import cors from 'cors';
 import { errors } from 'celebrate';
 
 import AppError from '@shared/errors/AppError';
+import rateLimiter from './middlewares/rateLimiter';
+
 import routes from './routes';
 
 import '@shared/infra/typeorm';
 import '@shared/container';
 
 const app = express();
+
+app.use(rateLimiter);
+app.use(cors());
 app.use(express.json());
-
 app.use(routes);
-
 app.use(errors());
-
 app.use(
   (error: Error, request: Request, response: Response, _: NextFunction) => {
     if (error instanceof AppError) {
